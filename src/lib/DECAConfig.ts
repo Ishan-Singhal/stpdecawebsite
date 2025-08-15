71d6ae3bcd33f51bc4b424057d94af3e7e1cc3d8
Ishan Singhal <ishansinghal123@gmail.com>
2025-08-15 01:05:54 -0500

    adminGSFixes

--- src/lib/DECAConfig.ts
+++ src/lib/DECAConfig.ts
@@ -4,7 +4,7 @@ class DECAConfig {
   private static readonly CACHE_DURATION = 5 * 60 * 1000; // 5 minutes
 
   // Initial URL - this will be updated from the Google Apps Script itself
-  private static readonly INITIAL_URL = 'https://script.google.com/macros/s/AKfycbzhydu9etrHTamRCK0l9Vn7Fdp5-2rjFQ0vmDU1POuktNXdV1OKECnnSRvMZ_v9japcdw/exec';
+  private static readonly INITIAL_URL = 'https://script.google.com/macros/s/AKfycby0LGwZFHlTM0eC1nNnD6WRCoP-HCMJ8KDavLBdPuMEsrbwow7YOZ73l4ApPPJqTx8J-A/exec';
 
   // Fetch configuration from Google Apps Script
   static async fetchRemoteConfig(): Promise<any> {
